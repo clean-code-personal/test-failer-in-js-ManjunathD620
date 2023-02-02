2d96a8e22edf335d3f203aeaf9031bae32062301
Manjunath D <82793670+ManjunathD620@users.noreply.github.com>
2023-02-02 21:21:19 +0530

    comparing acutual and expected output
    Capturing Console output and comparing with Expected output .

--- misaligned.js
+++ misaligned.js
@@ -13,4 +13,47 @@ function print_color_map() {
 
 result = print_color_map();
 expect(result).equals(25);
+
+const expectedOutput = [
+    "1 | White | Blue",
+    "2 | White | Orange",
+    "3 | White | Green",
+    "4 | White | Brown",
+    "5 | White | Slate",
+    "6 | Red | Blue",
+    "7 | Red | Orange",
+"8 | Red | Green",
+"9 | Red | Brown",
+"10 | Red | Slate",
+"11 | Black | Blue",
+"12 | Black | Orange",
+"13 | Black | Green",
+"14 | Black | Brown",
+"15 | Black | Slate",
+"16 | Yellow | Blue",
+"17 | Yellow | Orange",
+"18 | Yellow | Green",
+"19 | Yellow | Brown",
+"20 | Yellow | Slate",
+"21 | Violet | Blue",
+"22 | Violet | Orange",
+"23 | Violet | Green",
+"24 | Violet | Brown",
+"25 | Violet | Slate"
+  ];
+
+  
+  let capturedOutput = [];
+  const consoleLog = console.log;
+  console.log = (...args) => {
+    capturedOutput.push(args.join(" "));
+  };
+  
+  print_color_map();
+  
+  expect(capturedOutput).deep.equals(expectedOutput);
+  
+  console.log = consoleLog;
+
+
 console.log('All is well (maybe!)');
