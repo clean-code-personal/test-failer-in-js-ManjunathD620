d58be4e25f2a2e54df9badd4c3efee691468ef2f
Manjunath D <82793670+ManjunathD620@users.noreply.github.com>
2023-02-06 14:02:59 +0530

    Understood how to write test cases.
    Here, The values are fixed, We know the input and expected output. To check for dynamic values, we should need to have a right program and compare the outputs of right program and user program.

--- alerter.js
+++ alerter.js
@@ -6,9 +6,7 @@ function networkAlertStub(celcius) {
     // Return 200 for ok
     // Return 500 for not-ok
     // stub always succeeds and returns 200
-    if(celcius < 7)
-        return 500;
-    return 200;
+    return 500;
 }
 
 function alertInCelcius(farenheit) {
@@ -19,13 +17,13 @@ function alertInCelcius(farenheit) {
         // let us keep a count of failures to report
         // However, this code doesn't count failures!
         // Add a test below to catch this bug. Alter the stub above, if needed.
-        alertFailureCount += 1;
+        alertFailureCount += 0;
     }
 }
 
 alertInCelcius(400.5);
 alertInCelcius(303.6);
 alertInCelcius(-103.6);
-expect(alertFailureCount).equal(0);
+expect(alertFailureCount).equal(3);
 console.log(`${alertFailureCount} alerts failed.`);
 console.log('All is well (maybe!)');
