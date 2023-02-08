1bba195ea1917110ab4b3f4b61620fcb14e0c845
Manjunath D <82793670+ManjunathD620@users.noreply.github.com>
2023-02-08 11:16:02 +0530

    Spliting files into testing and production code

--- tshirts.js
+++ tshirts.js
@@ -1,6 +1,4 @@
-const {expect} = require('chai');
-
-function size(cms) {
+module.exports = function(cms) {
     if (cms < 38) {
         return 'S';
     } else if (cms > 38 && cms < 42) {
@@ -8,12 +6,5 @@ function size(cms) {
     } else {
         return 'L';
     }
-}
+};
 
-// expect(size(37)).equals('S');
-// expect(size(40)).equals('M');
-// expect(size(43)).equals('L');
-expect(size(38)).equals('S');
-expect(size(38)).equals('M');
-expect(size(38)).not.equals('L');
-console.log('All is well (maybe!)');
