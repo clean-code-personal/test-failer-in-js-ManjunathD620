f2a86e7328db320ae83b765edda376fa00639ebf
Manjunath D <82793670+ManjunathD620@users.noreply.github.com>
2023-02-08 14:43:07 +0530

    Testing Tshirt file

--- test/Test_tshirt.js
+++ test/Test_tshirt.js
@@ -7,5 +7,13 @@ describe("Tshirts",function(){
   it("Size is not available",function(){
     assert.notEqual(size(38),'L');
   });
+  
+  it("Size is not Available",function(){
+    assert.equal(size(-95),undefined)
+  });
+  
+  it("Size should be less than 100 ",function(){
+    assert.equal(size(259),undefined)
+  });
 
 });
