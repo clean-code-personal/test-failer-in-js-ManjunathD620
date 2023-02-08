12680364a3a38732f5ef76b87648a01793be399f
Manjunath D <82793670+ManjunathD620@users.noreply.github.com>
2023-02-08 14:54:30 +0530

    Solved separator ( | ) are misaligned problem

--- misaligned.js
+++ misaligned.js
@@ -3,7 +3,7 @@ function print_color_map() {
     const minorColors = ["Blue", "Orange", "Green", "Brown", "Slate"];
     for (let i = 0; i < majorColors.length; i++) {
         for (let j = 0; j < minorColors.length; j++) {
-            console.log(`${i * 5 + j} | ${majorColors[i]} | ${minorColors[j]}`);
+            console.log(`${(i * 5 + j).toString().padEnd(2)} | ${majorColors[i].padEnd(6)} | ${minorColors[j]}`);
         }
     }
     return majorColors.length * minorColors.length;
